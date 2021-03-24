b14ac03908e9206ac74b53216d44acaf6afee9cc
Алексей <soadist92@yandex.ru>
2021-03-24 12:24:01 +0300

    added serializers

---
+++ api/serializers.py
@@ -0,0 +1,29 @@
+from django.contrib.auth import get_user_model
+from rest_framework import serializers
+
+from .models import Comment, Review
+
+
+User = get_user_model()
+
+
+class ReviewSerializer(serializers.ModelSerializer):
+    author = serializers.SlugRelatedField(
+        slug_field='username',
+        read_only=True,
+    )
+
+    class Meta:
+        exclude = ['title']
+        model = Review
+
+
+class CommentSerializer(serializers.ModelSerializer):
+    author = serializers.SlugRelatedField(
+        slug_field='username',
+        read_only=True,
+    )
+
+    class Meta:
+        model = Comment
+        exclude = ['review']
