31a939ff4c5edd27e6fb7d28c8e5d4b85799b452
Алексей <soadist92@yandex.ru>
2021-03-24 12:24:18 +0300

    changed urls

---
+++ api/urls.py
@@ -0,0 +1,26 @@
+from django.urls import include, path
+from rest_framework.routers import DefaultRouter
+from rest_framework_simplejwt.views import (TokenObtainPairView,
+                                            TokenRefreshView)
+from .views import ReviewsViewSet, CommentsViewSet
+
+router = DefaultRouter(trailing_slash=True)
+router.register(
+    r'titles/(?P<title_id>\d+)/reviews',
+    ReviewsViewSet,
+    basename='reviews'
+)
+router.register(
+    r'titles/(?P<title_id>\d+)/reviews/(?P<review_id>\d+)/comments',
+    CommentsViewSet,
+    basename='comments'
+)
+
+urlpatterns = [
+    path('v1/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path(
+        'v1/token/refresh/',
+        TokenRefreshView.as_view(), name='token_refresh'
+    ),
+    path('v1/', include(router.urls)),
+]
